fd50985c90d5a0902c9798186ef233fd6092b67b
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2024-09-06 19:28:33 +0700

    Init ReactJs proejct

--- food_delivery_react/src/main.jsx
+++ food_delivery_react/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react' 
 import App from './App.jsx'
 import './index.css'
 
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App></App>
+    ),
+  }, 
+]);
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
