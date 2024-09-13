591537f6e0f6be0a2e5686c82fb4a243b7d12676
nguyenHung <nguyenhung1572003@gmail.com>
2024-09-14 00:49:25 +0700

    ➕[Create] RestaurantEdit

--- food_delivery_react/src/components/Restaurant/RestaurantEdit.jsx
+++ food_delivery_react/src/components/Restaurant/RestaurantEdit.jsx
@@ -7,26 +7,42 @@ import { useParams } from 'react-router-dom';
 
 const RestaurantEdit = () => {
     const [restaurant, setRestaurant] = useState({});
-    const [foodNextPage, setFoodNextPage] = useState('');
+    const [employee, setEmployee] = useState([]);
     const BASE_URL = import.meta.env.VITE_BASE_URL;
     const { id } = useParams();
+    const [selecting, setSelecting] = useState();
 
     useEffect(() => {
-        getFoods();
-        console.log(id);
+        getEmployees();
+        getRestaurant();
+        console.log(employee)
     }, [])
 
-    const getFoods = async () => {
+    useEffect(() => {
+        console.log(selecting);
+    }, [selecting])
+
+    const getRestaurant = async () => {
         const response = await fetch(BASE_URL + '/restaurants/' + id, {
             method: 'GET',
         })
 
         if (response.status == 200) {
             const data = await response.json();
-            console.log(data);
             setRestaurant(data);
         }
+    }
 
+    const getEmployees = async () => {
+        const response = await fetch(BASE_URL + '/restaurants/' + id + '/employees/', {
+            method: 'GET',
+        })
+
+        if (response.status == 200) {
+            const data = await response.json();
+            console.log(data)
+            setEmployee(data)
+        }
     }
 
     function round(value, step) {
@@ -36,8 +52,8 @@ const RestaurantEdit = () => {
     }
 
     return (
-        <div className='w-full h-full p-2'>
-            <div className='bg-white py-12'>
+        <div className='w-full h-full py-4 px-8'>
+            <div className='bg-white py-12 w-3/4 mx-auto'>
                 <div className='container mx-auto'>
                     <h1 className='text-4xl my-4 font-semibold'>{restaurant?.name}</h1>
                     <p className='my-4'>{restaurant?.category}</p>
@@ -52,10 +68,10 @@ const RestaurantEdit = () => {
 
             <div className='w-full flex'>
                 <div className='w-1/5'>
-                    <SideBar />
+                    <SideBar handleSelect={(barItem) => setSelecting(barItem)} />
                 </div>
                 <div className='w-4/5'>
-                    <BoardView foods={restaurant?.foods} />
+                    <BoardView foods={restaurant?.foods} employees={employee} selecting={selecting} />
                 </div>
             </div>
         </div>
--- food_delivery_react/src/components/Restaurant/SideBar.jsx
+++ food_delivery_react/src/components/Restaurant/SideBar.jsx
@@ -14,32 +14,7 @@ const linkData = [
     },
 ]
 
-const SideBar = () => {
-    const Card = ({ name, price }) => {
-        return (
-            <div className='w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-between'>
-                <div className='h-full flex flex-1 flex-col justify-between'>
-                    <p className='text-base text-gray-600'>{name}</p>
-                    <span className='text-2xl font-semibold'>{price}</span>
-                </div>
-
-                <Button
-                    className='text-blue-600 hover:text-blue-500 font-semibold sm:px-0'
-                    label='Edit'
-                    type='button'
-                    onClick={() => editClick(user)}
-                />
-
-                <Button
-                    className='text-red-700 hover:text-red-500 font-semibold sm:px-0'
-                    label='Delete'
-                    type='button'
-                    onClick={() => deleteClick(user?._id)}
-                />
-            </div>
-        )
-    }
-
+const SideBar = ({ handleSelect }) => {
     return (
         <div className='w-full h-full flex flex-col gap-6 p-5'>
             <h1 className='flex gap-1 items-center'>
@@ -47,7 +22,12 @@ const SideBar = () => {
             </h1>
             <div className=''>
                 {linkData.map((data, index) => (
-                    <div className='flex items-center gap-3 hover:bg-gray-200 py-4 px-2'>
+                    <div
+                        className='flex items-center gap-3 hover:bg-gray-200 py-4 px-2'
+                        onClick={() => {
+                            handleSelect(data.label);
+                        }}
+                    >
                         {data?.icon}
                         <p className='text-lg'>{data?.label}</p>
                     </div>
